a8a49da61ec3ead17b765b288ba78596a8aeeaad
ZHX-GIT <62171180+ZHX-GIT@users.noreply.github.com>
2020-03-22 18:01:11 +0900

    k.chou test svn

--- src/hw/HelloWorld.java
+++ src/hw/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		/*
-		 * K.Chou test 17：25
+		 * K.Chou test 18：00 svn
 		 *
 		 */
 		System.out.println("Hello World Tom");
